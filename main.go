93880c7a8e262e46c35963985530198c25f1eeef
Olivia Bahr <ob.bahr@gmail.com>
2024-11-20 16:17:21 -0700

    Add flags

--- main.go
+++ main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"os"
 	"tipJar/globals/log"
 	"tipJar/ui"
 
+	charmLog "github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
+
+var stdLog = charmLog.New(os.Stdout)
 
 var rootCmd = &cobra.Command{
 	Use:   "tipJar",
 	Short: "tipJar is a CLI tool for managing notes",
 	Run: func(cmd *cobra.Command, args []string) {
-		err := ui.RunUI()
+		logLevel := charmLog.DebugLevel // default level
+		switch {
+		case cmd.Flag("debug").Changed:
+			logLevel = charmLog.DebugLevel
+		case cmd.Flag("info").Changed:
+			logLevel = charmLog.InfoLevel
+		case cmd.Flag("warn").Changed:
+			logLevel = charmLog.WarnLevel
+		case cmd.Flag("error").Changed:
+			logLevel = charmLog.ErrorLevel
+		}
+		// init global logger
+		err := log.InitializeLogger(cmd.Flag("log-to").Value.String(), logLevel)
 		if err != nil {
-			log.Fatal("Main UI failed", "e", err)
+			stdLog.Fatal("failed to initialize logger", "e", err)
+		}
+		// enter ui
+		err = ui.RunUI()
+		if err != nil {
+			stdLog.Fatal("Main UI failed", "e", err)
 		}
 	},
 }
@@ -21,18 +42,28 @@ var rootCmd = &cobra.Command{
 func Execute() error {
 	err := rootCmd.Execute()
 	if err != nil {
-		log.Error("cobra Root command failed", "e", err)
+		stdLog.Error("cobra Root command failed", "e", err)
 		return err
 	}
 	return nil
 }
 
-func init() {}
+func init() {
+	rootCmd.Flags().StringP("log-to", "l", ".tmp/log.log", "stream or file to log to")
+
+	// Add boolean flags for each log level
+	rootCmd.Flags().BoolP("debug", "d", false, "set log level to debug")
+	rootCmd.Flags().BoolP("info", "i", false, "set log level to info")
+	rootCmd.Flags().BoolP("warn", "w", false, "set log level to warn")
+	rootCmd.Flags().BoolP("error", "e", false, "set log level to error")
+
+	// Make the log level flags mutually exclusive
+	rootCmd.MarkFlagsMutuallyExclusive("debug", "info", "warn", "error")
+}
 
 func main() {
-	log.Info("===== starting tipJar =====")
 	err := Execute()
 	if err != nil {
-		log.Fatal("fatal error", "e", err)
+		stdLog.Fatal("fatal error", "e", err)
 	}
 }
